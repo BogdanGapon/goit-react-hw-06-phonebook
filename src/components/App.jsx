99a453d5672ddb475ae20c93691b690c94b1385a
Bogdan <bogdangaponenko1996@gmail.com>
2023-05-10 00:34:35 -0500

    cannot filter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,9 @@
-import { useState } from 'react';
 import { AppWrapper } from './App.styled';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactFilter } from './ContactFilter/ContactFilter';
 import { ContactList } from './ContactList/ContactList';
-// import { useLocalStorage } from 'hooks/useLocalStorage';
+
 export const App = () => {
-  //   const [contacts, setContacts] = useState([]);
-  //   const [filter, setFilter] = useState('');
-
-  // const onDeleteContact = contactId => {
-  //   setContacts(prevState =>
-  //     prevState.filter(contact => contact.id !== contactId)
-  //   );
-  // };
-
-  // const changeContactsBySumbit = obj => {
-  //   setContacts(prevState => [obj, ...prevState]);
-  // };
-
-  // const handleFindContacts = evt => {
-  //   setFilter(evt.currentTarget.value);
-  // };
-
   return (
     <AppWrapper>
       <h2>Phonebook</h2>
